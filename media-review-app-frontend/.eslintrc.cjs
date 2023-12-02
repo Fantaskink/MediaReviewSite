0e125099c4727ffe5a802931b5820961ea006642
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2023-12-02 15:07:19 +0100

    Modify ESLint behaviour

--- media-review-app-frontend/.eslintrc.cjs
+++ media-review-app-frontend/.eslintrc.cjs
@@ -15,6 +15,8 @@ module.exports = {
       { allowConstantExport: true },
     ],
     'quotes': ['error', 'single'],
+    // we want to force semicolons
+    'semi': ['error', 'never'],
     // we use 2 spaces to indent our code
     'indent': ['error', 2],
     // we want to avoid extraneous spaces
