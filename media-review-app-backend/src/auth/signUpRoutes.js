52ae046934e4253d82ce664d225099e68d9a028e
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2023-12-07 15:41:02 +0100

    Update signUpRoutes.js

--- media-review-app-backend/src/auth/signUpRoutes.js
+++ media-review-app-backend/src/auth/signUpRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const signUpController = require('./auth/signUpController');
+const signUpController = require('./signUpController');
 
 router.post('/signup', (req, res) => {
     const { userName, email, password } = req.body;
